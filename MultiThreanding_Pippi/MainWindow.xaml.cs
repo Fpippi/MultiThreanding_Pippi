41587e30428a34019952bf7d9d112a82d786952c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-23 09:14:21 +0200

    Creazione thread

--- MultiThreanding_Pippi/MainWindow.xaml.cs
+++ MultiThreanding_Pippi/MainWindow.xaml.cs
@@ -20,9 +20,32 @@ namespace MultiThreanding_Pippi
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Btn_task_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_risultato.Content = "Risultato = ";
+            Task.Factory.StartNew(Dowork);
+        }
+
+        public void Dowork()
+        {
+            for (int i = 0; i <= 100000; i++)
+            {
+                for (int j = 0; j <= 100000; j++)
+                {
+                }
+            }
+            Dispatcher.Invoke(AggiornaInterfaccia);
+        }
+
+        private void AggiornaInterfaccia()
+        {
+            lbl_risultato.Content += "Finito";
         }
     }
 }
